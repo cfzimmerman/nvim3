4bd38d80db1cebdcbbc88b95517fc4cfb8d05690
Cory Zimmerman <coryzimmerman93@gmail.com>
2026-08-06 18:54:56 -0500

    Add jk

--- init.lua
+++ init.lua
@@ -211,6 +211,8 @@ do
 
   vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+  vim.keymap.set('i', 'jk', '<Esc>')
+
   -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
   -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
   -- is not what someone will guess without a bit more experience.
