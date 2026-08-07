73f130cdc4335d348cb5ea2c30710e0e62f770d6
Cory Zimmerman <coryzimmerman93@gmail.com>
2026-08-06 20:42:55 -0500

    add diff viewer

--- init.lua
+++ init.lua
@@ -447,8 +447,13 @@ do
   ---@diagnostic disable-next-line: duplicate-set-field
   statusline.section_location = function() return '%2l:%-2v' end
 
-  -- ... and there is more!
-  --  Check out: https://github.com/nvim-mini/mini.nvim
+  -- Git diff viewer
+  vim.pack.add { gh 'sindrets/diffview.nvim' }
+  require('diffview').setup {
+    default_args = {
+      DiffviewOpen = { "--imply-local" },
+    }
+  }
 end
 
 -- ============================================================
